133fede42fb0891fcb51c810aeb000a88715be5a
Sandra Alvarez <sandruskiag@gmail.com>
2018-09-19 21:57:53 +0200

    Building the Game (Tutorial 8 - Final)

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -46,7 +46,7 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("Pick Up"))
         {
             count++;
             SetCountText();
