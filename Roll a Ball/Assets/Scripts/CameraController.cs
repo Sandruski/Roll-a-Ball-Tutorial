c66c2a1466379dbee8c208dd3da91498287d4372
Sandra Alvarez <sandruskiag@gmail.com>
2018-09-19 18:36:36 +0200

    Moving the Camera (Tutorial 3)

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region PUBLIC_VARIABLES
+    public GameObject player;
+    #endregion
+
+    #region PRIVATE_VARIABLES
+    private Vector3 offset;
+    #endregion
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+	}
+}
